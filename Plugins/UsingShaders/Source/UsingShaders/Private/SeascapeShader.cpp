c65cbc36254b3d51e02356de10cd72adcfc8eb11
haiaimi <wanghai1997@outlook.com>
2019-07-16 21:48:38 +0800

    添加Seascape，Shader 有待改善

--- Plugins/UsingShaders/Source/UsingShaders/Private/SeascapeShader.cpp
+++ Plugins/UsingShaders/Source/UsingShaders/Private/SeascapeShader.cpp
@@ -1,4 +1,4 @@
-#include "UniformBufferShader.h"
+#include "SeascapeShader.h"
 #include "RHIResources.h"
 #include "RenderResource.h"
 #include "GlobalShader.h"
@@ -14,24 +14,22 @@
 #include "SceneInterface.h"
 #include "Button.h"
 
-BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FUniformBufferShaderData, )
-SHADER_PARAMETER(FVector4, ColorOne)
-SHADER_PARAMETER(FVector4, ColorTwo)
-SHADER_PARAMETER(FVector4, ColorThree)
-SHADER_PARAMETER(FVector4, ColorFour)
-SHADER_PARAMETER(uint32, ColorIndex)
+BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FSeascapeShaderData, )
+SHADER_PARAMETER(FVector2D, ViewResolution)
+SHADER_PARAMETER(float, TimeSeconds)
+SHADER_PARAMETER(FVector2D, MousePos)
 END_GLOBAL_SHADER_PARAMETER_STRUCT()
 
  //为Shader声明UniformBuffer，不需要在Shader中声明，该声明会把这些定义写入到Common.ush的shader文件中
-IMPLEMENT_GLOBAL_SHADER_PARAMETER_STRUCT(FUniformBufferShaderData, "FUniformData");    
+IMPLEMENT_GLOBAL_SHADER_PARAMETER_STRUCT(FSeascapeShaderData, "FUniformData");    
 
-struct FUVertexInput
+struct FSVertexInput
 {
 	FVector4 Position;
 	FVector2D UV;
 };
 
-class FUniformBufferDeclaration : public FRenderResource
+class FSeascapeVertexDeclaration : public FRenderResource
 {
 public:
 	FVertexDeclarationRHIRef VertexDeclarationRHI;
@@ -40,9 +38,9 @@ public:
 	{
 		//设置顶点输入布局
 		FVertexDeclarationElementList Elements;
-		uint32 Stride = sizeof(FUVertexInput);   //步长
-		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FUVertexInput, Position), EVertexElementType::VET_Float4, 0, Stride));  //第一个参数Stream应该是配合Instance来使用
-		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FUVertexInput, UV), EVertexElementType::VET_Float2, 1, Stride));
+		uint32 Stride = sizeof(FSVertexInput);   //步长
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FSVertexInput, Position), EVertexElementType::VET_Float4, 0, Stride));  //第一个参数Stream应该是配合Instance来使用
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FSVertexInput, UV), EVertexElementType::VET_Float2, 1, Stride));
 		VertexDeclarationRHI = RHICreateVertexDeclaration(Elements);
 	}
 
@@ -52,17 +50,15 @@ public:
 	}
 };
 
-class FUniformBufferShader : public FGlobalShader
+class FSeascapeShader : public FGlobalShader
 {
 public:
-	FUniformBufferShader() {}
+	FSeascapeShader() {}
 
-	FUniformBufferShader(const ShaderMetaType::CompiledShaderInitializerType& Initializer) :
+	FSeascapeShader(const ShaderMetaType::CompiledShaderInitializerType& Initializer) :
 		FGlobalShader(Initializer)
 	{
-		SimpleColorVal.Bind(Initializer.ParameterMap, TEXT("SimpleColor"));
-		TestTextureVal.Bind(Initializer.ParameterMap, TEXT("UniformBufferTexture"));
-		TestTextureSampler.Bind(Initializer.ParameterMap, TEXT("UniformBufferTextureSampler"));
+
 	}
 
 	static bool ShouldCache(EShaderPlatform Platform)
@@ -78,75 +74,68 @@ public:
 	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
 	{
 		FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
-		OutEnvironment.SetDefine(TEXT("UNIFORM_BUFFER_MICRO"), 1);
+		OutEnvironment.SetDefine(TEXT("SEASCAPE_MICRO"), 1);
 	}
 
-	void SetParameters(FRHICommandListImmediate& RHICmdList, const FLinearColor& MyColor, FTextureRHIParamRef& MyTexture, FUniformBufferData& MyData)
+	void SetParameters(FRHICommandListImmediate& RHICmdList, FSeascapeBufferData& MyData)
 	{
 		//设置一个普通变量
-		SetShaderValue(RHICmdList, GetPixelShader(), SimpleColorVal, MyColor);
+		//SetShaderValue(RHICmdList, GetPixelShader(), SimpleColorVal, MyColor);
 		 
 		//下面是为着色器设置纹理以及纹理参数
-		SetTextureParameter(RHICmdList, GetPixelShader(), TestTextureVal, TestTextureSampler, 
-				TStaticSamplerState<SF_AnisotropicLinear, AM_Clamp, AM_Clamp, AM_Clamp>::GetRHI(),     //纹理采样配置，三线性过滤，U，V,W三个维度都是Clamp
-				MyTexture);
+		//SetTextureParameter(RHICmdList, GetPixelShader(), TestTextureVal, TestTextureSampler, 
+		//		TStaticSamplerState<SF_AnisotropicLinear, AM_Clamp, AM_Clamp, AM_Clamp>::GetRHI(),     //纹理采样配置，三线性过滤，U，V,W三个维度都是Clamp
+		//		MyTexture);
 		//设置UniformBuffer
-		FUniformBufferShaderData UBShaderData;
-		UBShaderData.ColorOne = MyData.ColorOne;
-		UBShaderData.ColorTwo = MyData.ColorTwo;
-		UBShaderData.ColorThree = MyData.ColorThree;
-		UBShaderData.ColorFour = MyData.ColorFour;
-		UBShaderData.ColorIndex = MyData.ColorIndex;
+		FSeascapeShaderData SShaderData;
+		SShaderData.ViewResolution = MyData.ViewResolution;
+		SShaderData.TimeSeconds = MyData.TimeSeconds;
+		SShaderData.MousePos = MyData.MousePos;
 
-		SetUniformBufferParameter(RHICmdList, GetPixelShader(), GetUniformBufferParameter<FUniformBufferShaderData>(), FUniformBufferShaderData::CreateUniformBuffer(UBShaderData, EUniformBufferUsage::UniformBuffer_SingleDraw));
+		SetUniformBufferParameter(RHICmdList, GetPixelShader(), GetUniformBufferParameter<FSeascapeShaderData>(), FSeascapeShaderData::CreateUniformBuffer(SShaderData, EUniformBufferUsage::UniformBuffer_SingleDraw));
 	}
 
 	virtual bool Serialize(FArchive& Ar) override
 	{
 		bool bShaderHasOutdatedParameters = FGlobalShader::Serialize(Ar);
-		Ar << SimpleColorVal << TestTextureVal;
+		//Ar << SimpleColorVal << TestTextureVal;
 		return bShaderHasOutdatedParameters;
 	}
 
 private:
-	FShaderParameter SimpleColorVal;
-
-	FShaderResourceParameter TestTextureVal;
-
-	FShaderResourceParameter TestTextureSampler;
 };
 
-class FUniformBufferShaderVS :public FUniformBufferShader
+class FSeascapeShaderVS :public FSeascapeShader
 {
-	DECLARE_SHADER_TYPE(FUniformBufferShaderVS, Global);
+	DECLARE_SHADER_TYPE(FSeascapeShaderVS, Global);
 
 public:
-	FUniformBufferShaderVS() {}
+	FSeascapeShaderVS() {}
 
-	FUniformBufferShaderVS(const ShaderMetaType::CompiledShaderInitializerType& Initializer):
-		FUniformBufferShader(Initializer)
+	FSeascapeShaderVS(const ShaderMetaType::CompiledShaderInitializerType& Initializer):
+		FSeascapeShader(Initializer)
 	{
 	}
 };
 
-class FUniformBufferShaderPS :public FUniformBufferShader
+class FSeascapeShaderPS :public FSeascapeShader
 {
-	DECLARE_SHADER_TYPE(FUniformBufferShaderPS, Global);
+	DECLARE_SHADER_TYPE(FSeascapeShaderPS, Global);
 
 public:
-	FUniformBufferShaderPS() {}
+	FSeascapeShaderPS() {}
 
-	FUniformBufferShaderPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer):
-		FUniformBufferShader(Initializer)
+	FSeascapeShaderPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer):
+		FSeascapeShader(Initializer)
 	{
 	}
 };
 
-IMPLEMENT_SHADER_TYPE(, FUniformBufferShaderVS, TEXT("/Plugins/Shaders/Private/UniformBufferShader.usf"),TEXT("MainVS"), SF_Vertex)
-IMPLEMENT_SHADER_TYPE(, FUniformBufferShaderPS, TEXT("/Plugins/Shaders/Private/UniformBufferShader.usf"),TEXT("MainPS"), SF_Pixel)
+IMPLEMENT_SHADER_TYPE(, FSeascapeShaderVS, TEXT("/Plugins/Shaders/Private/SeascapeShader.usf"),TEXT("MainVS"), SF_Vertex)
+IMPLEMENT_SHADER_TYPE(, FSeascapeShaderPS, TEXT("/Plugins/Shaders/Private/SeascapeShader.usf"),TEXT("MainPS"), SF_Pixel)
 
 
-static void DrawIndexedPrimitiveUP_cpy3(
+static void DrawIndexedPrimitiveUP_cpy4(
 	FRHICommandList& RHICmdList,
 	uint32 PrimitiveType,
 	uint32 MinVertexIndex,
@@ -183,9 +172,7 @@ static void DrawUniformBufferShaderRenderTarget_RenderThread(
 	FTextureRenderTargetResource* OutputRenderTargetResource,
 	ERHIFeatureLevel::Type FeatureLevel,
 	FName TextureRenderTargetName,
-	FLinearColor MyColor,
-	FTextureRHIParamRef MyTexture,
-	FUniformBufferData MyData
+	FSeascapeBufferData MyData
 )
 {
 	check(IsInRenderingThread())
@@ -198,23 +185,17 @@ static void DrawUniformBufferShaderRenderTarget_RenderThread(
 	SCOPED_DRAW_EVENTF(RHICmdList, DrawUVDisplacementRenderTarget_RenderThread);
 #endif
 
-	/*SetRenderTarget(RHICmdList,
-		OutputRenderTargetResource->GetRenderTargetTexture(),
-		FTextureRHIRef(),
-		ESimpleRenderTargetMode::EUninitializedColorAndDepth,
-		FExclusiveDepthStencil::DepthNop_StencilNop);*/
-	//RHICmdList.BeginRenderPass(FRHIRenderPassInfo(OutputRenderTargetResource->GetRenderTargetTexture(), EDepthStencilTargetActions::ClearDepthStencil_DontStoreDepthStencil, nullptr, FExclusiveDepthStencil::DepthNop_StencilNop), TEXT("hello"));
 	FIntPoint DrawTargetResolution(OutputRenderTargetResource->GetSizeX(), OutputRenderTargetResource->GetSizeY());  
     RHICmdList.SetViewport(0, 0, 0.0f, 500, 500, 1.0f);    //设置视口大小
 
 	FRHIRenderPassInfo RPInfo(OutputRenderTargetResource->GetRenderTargetTexture(), ERenderTargetActions::DontLoad_Store, OutputRenderTargetResource->TextureRHI);
-	RHICmdList.BeginRenderPass(RPInfo, TEXT("UniformBufferShader"));
+	RHICmdList.BeginRenderPass(RPInfo, TEXT("SeascapeShader"));
 
 	TShaderMap<FGlobalShaderType>* GlobalShaderMap = GetGlobalShaderMap(FeatureLevel);
-	TShaderMapRef<FUniformBufferShaderVS> VertexShader(GlobalShaderMap);
-	TShaderMapRef<FUniformBufferShaderPS> PixelShader(GlobalShaderMap);        //获取自定义的Shader
+	TShaderMapRef<FSeascapeShaderVS> VertexShader(GlobalShaderMap);
+	TShaderMapRef<FSeascapeShaderPS> PixelShader(GlobalShaderMap);        //获取自定义的Shader
 
-	FUniformBufferDeclaration VertexDeclaration;   
+	FSeascapeVertexDeclaration VertexDeclaration;   
 	VertexDeclaration.InitRHI(); //创建定点输入布局
 
 	FGraphicsPipelineStateInitializer GraphicPSPoint;
@@ -228,9 +209,9 @@ static void DrawUniformBufferShaderRenderTarget_RenderThread(
 	GraphicPSPoint.BoundShaderState.PixelShaderRHI = GETSAFERHISHADER_PIXEL(*PixelShader);
 	SetGraphicsPipelineState(RHICmdList, GraphicPSPoint);
 
-	PixelShader->SetParameters(RHICmdList, MyColor, MyTexture, MyData);    //设置Shader参数，这里一个颜色参数和一个纹理参数
+	PixelShader->SetParameters(RHICmdList, MyData);    //设置Shader参数，这里一个颜色参数和一个纹理参数
 
-	FUVertexInput Vertices[4];
+	FSVertexInput Vertices[4];
 	Vertices[0].Position.Set(-1.0f, 1.0f, 0, 1.0f);
 	Vertices[1].Position.Set(1.0f, 1.0f, 0, 1.0f);
 	Vertices[2].Position.Set(-1.0f, -1.0f, 0, 1.0f);
@@ -245,32 +226,31 @@ static void DrawUniformBufferShaderRenderTarget_RenderThread(
         2, 1, 3  
     };  
 
-	DrawIndexedPrimitiveUP_cpy3(RHICmdList, PT_TriangleList, 0, ARRAY_COUNT(Vertices), 2, Indices, sizeof(Indices[0]), Vertices, sizeof(Vertices[0]));
-	//RHICmdList.CopyToResolveTarget(OutputRenderTargetResource->GetRenderTargetTexture(), OutputRenderTargetResource->TextureRHI, FResolveParams());
+	DrawIndexedPrimitiveUP_cpy4(RHICmdList, PT_TriangleList, 0, ARRAY_COUNT(Vertices), 2, Indices, sizeof(Indices[0]), Vertices, sizeof(Vertices[0]));
+	RHICmdList.CopyToResolveTarget(OutputRenderTargetResource->GetRenderTargetTexture(), OutputRenderTargetResource->TextureRHI, FResolveParams());
 
 	RHICmdList.EndRenderPass();
 }
 
 
-UUniformShaderBlueprintLibrary::UUniformShaderBlueprintLibrary(const FObjectInitializer& ObjectInitializer)
+USeascapeShaderBlueprintLibrary::USeascapeShaderBlueprintLibrary(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 }
 
-void UUniformShaderBlueprintLibrary::DrawUniformBufferShaderRenderTarget(class UTextureRenderTarget* OutputRenderTarget, AActor* Ac, FLinearColor MyColor, class UTexture* MyTexture, FUniformBufferData MyData)
+void USeascapeShaderBlueprintLibrary::DrawSeascapeShaderRenderTarget(class UTextureRenderTarget* OutputRenderTarget, AActor* MyActor, FSeascapeBufferData ShaderStructData)
 {
 	check(IsInGameThread());
 
 	if (!OutputRenderTarget)return;
 
 	FTextureRenderTargetResource* TextureRenderTargetResource = OutputRenderTarget->GameThread_GetRenderTargetResource();
-	FTextureRHIParamRef MyTextureRHI = MyTexture->TextureReference.TextureReferenceRHI;
-	UWorld* World = Ac->GetWorld();
+	UWorld* World = MyActor->GetWorld();
 	ERHIFeatureLevel::Type FeatureLevel = World->Scene->GetFeatureLevel();
 	FName TextureRenderTargetName = OutputRenderTarget->GetFName();
 	//把渲染加入到渲染线程
-	ENQUEUE_RENDER_COMMAND(CaptureCommand)([TextureRenderTargetResource, FeatureLevel, MyColor, TextureRenderTargetName, MyTextureRHI, MyData](FRHICommandListImmediate& RHICmdList)
+	ENQUEUE_RENDER_COMMAND(CaptureCommand)([TextureRenderTargetResource, FeatureLevel, TextureRenderTargetName, ShaderStructData](FRHICommandListImmediate& RHICmdList)
 	{
-		DrawUniformBufferShaderRenderTarget_RenderThread(RHICmdList, TextureRenderTargetResource, FeatureLevel, TextureRenderTargetName, MyColor, MyTextureRHI, MyData);
+		DrawUniformBufferShaderRenderTarget_RenderThread(RHICmdList, TextureRenderTargetResource, FeatureLevel, TextureRenderTargetName, ShaderStructData);
 	});
 }
