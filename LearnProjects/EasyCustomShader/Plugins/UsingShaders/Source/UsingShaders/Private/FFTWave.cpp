dd292064ae31b69426500d7b00a8ec3137858fa3
haiaimi <wanghai1997@outlook.com>
2019-11-27 01:50:04 +0800

    Add Shader parameter

--- LearnProjects/EasyCustomShader/Plugins/UsingShaders/Source/UsingShaders/Private/FFTWave.cpp
+++ LearnProjects/EasyCustomShader/Plugins/UsingShaders/Source/UsingShaders/Private/FFTWave.cpp
@@ -8,22 +8,32 @@
 #include "SceneInterface.h"
 #include "RHICommandList.h"
 #include "Engine/Engine.h"
-
-BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FWaveSimulateData, )
-SHADER_PARAMETER(FVector2D, ViewResolution)
-SHADER_PARAMETER(float, TimeSeconds)
-SHADER_PARAMETER(FVector2D, MousePos)
-END_GLOBAL_SHADER_PARAMETER_STRUCT()
+#include "Engine/Public/SceneView.h"
+#include "ShaderParameterStruct.h"
 
 class FWaveFFTComputeShader : public FGlobalShader
 {
 	DECLARE_SHADER_TYPE(FWaveFFTComputeShader, Global)
+
+	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER(FVector2D, ViewResolution)
+		SHADER_PARAMETER(float, TimeSeconds)
+		SHADER_PARAMETER(int32, WaveSize)
+		SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
+		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float2>, HeightBuffer)
+		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float4>, SlopeBuffer)
+		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float4>, DisplacementBuffer)
+		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float2>, Spectrum)
+	END_SHADER_PARAMETER_STRUCT()
+
 public:
+	
 	FWaveFFTComputeShader() {}
 
 	FWaveFFTComputeShader(const ShaderMetaType::CompiledShaderInitializerType& Initializer) :
 		FGlobalShader(Initializer)
 	{
+		ButterflyLookupTable.Bind(Initializer.ParameterMap, TEXT("ButterflyLookUpTable"));
 		InputSurface.Bind(Initializer.ParameterMap, TEXT("InputTexture"));
 		InputSampler.Bind(Initializer.ParameterMap, TEXT("TextureSampler"));
 		OutputSurface.Bind(Initializer.ParameterMap, TEXT("OutputTexture"));
@@ -45,11 +55,13 @@ public:
 		OutEnvironment.SetDefine(TEXT("BLUR_MICRO"), 1);
 	}
 
-	void SetParameters(FRHICommandListImmediate& RHICmdList, const FTextureRHIParamRef& InTexture,FUnorderedAccessViewRHIRef OutputUAV)
+	void SetParameters(FRHICommandListImmediate& RHICmdList, const FTextureRHIParamRef& InTexture, FUnorderedAccessViewRHIRef OutputUAV, const TArray<FVector4, TInlineAllocator<4>>& InButterflyLookupTable)
 	{
 		if (OutputSurface.IsBound())
 			RHICmdList.SetUAVParameter(GetComputeShader(), OutputSurface.GetBaseIndex(), OutputUAV);
 
+
+		SetShaderValueArray(RHICmdList, GetComputeShader(), ButterflyLookupTable, InButterflyLookupTable.GetData(), InButterflyLookupTable.Num());
 		SetTextureParameter(RHICmdList, GetComputeShader(), InputSurface, InputSampler, TStaticSamplerState<SF_AnisotropicLinear, AM_Clamp, AM_Clamp, AM_Clamp>::GetRHI(), InTexture);
 
 		//SetUniformBufferParameter(RHICmdList, GetPixelShader(), GetUniformBufferParameter<FBlurComputeShaderData>(), FBlurComputeShaderData::CreateUniformBuffer(BlurData, EUniformBufferUsage::UniformBuffer_SingleDraw));
@@ -72,6 +84,8 @@ public:
 	}
 
 private:
+	FShaderParameter ButterflyLookupTable;
+
 	FShaderResourceParameter InputSurface;
 
 	FShaderResourceParameter InputSampler;
