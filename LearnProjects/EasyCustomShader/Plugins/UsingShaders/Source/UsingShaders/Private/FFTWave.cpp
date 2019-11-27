8fcd00ad5d1625a929b9cc6b5ffc20f39682dac6
haiaimi <wanghai1997@outlook.com>
2019-11-27 22:23:33 +0800

    Add FFTWave Shader

--- LearnProjects/EasyCustomShader/Plugins/UsingShaders/Source/UsingShaders/Private/FFTWave.cpp
+++ LearnProjects/EasyCustomShader/Plugins/UsingShaders/Source/UsingShaders/Private/FFTWave.cpp
@@ -19,6 +19,7 @@ class FWaveFFTComputeShader : public FGlobalShader
 		SHADER_PARAMETER(FVector2D, ViewResolution)
 		SHADER_PARAMETER(float, TimeSeconds)
 		SHADER_PARAMETER(int32, WaveSize)
+		SHADER_PARAMETER(int32, StartIndex)
 		SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
 		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float2>, HeightBuffer)
 		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float4>, SlopeBuffer)
@@ -92,3 +93,70 @@ private:
 
 	FShaderResourceParameter OutputSurface;
 };
+
+int32 BitReverse(int32 i, int32 Size)
+{
+	int32 j = i;
+	int32 Sum = 0;
+	int32 W = 1;
+	int32 M = Size >> 1;
+	while (M!= 0)
+	{
+		j = ((i&M) > M - 1) ? 1 : 0;
+		Sum += j * W;
+		W <<= 1;
+		M >>= 1;
+	}
+	return Sum;
+}
+
+void ComputeButterflyLookuptable(int32 Size, int32 Passes, TArray<float>& OutTable)
+{
+	OutTable.Reset();
+	OutTable.SetNum(Size * Passes * 4);
+
+	for (int32 i = 0; i < Passes; ++i)
+	{
+		int32 Blocks = FMath::Pow(2, Passes - 1 - i);
+		int32 HInputs = FMath::Pow(2, i);
+
+		for (int32 j = 0; j < Blocks; ++j)
+		{
+			for (int32 k = 0; k < HInputs; ++k)
+			{
+				int32 i1, i2, j1, j2;
+				if (i == 0)
+				{
+					i1 = j * HInputs * 2 + k;
+					i2 = j * HInputs * 2 + HInputs + k;
+					j1 = BitReverse(i1, Size);
+					j2 = BitReverse(i2, Size);
+				}
+				else
+				{
+					i1 = j * HInputs * 2 + k;
+					i2 = j * HInputs * 2 + HInputs + k;
+					j1 = i1;
+					j2 = i2;
+				}
+
+				float WR = FMath::Cos(2.f * PI * float(k * Blocks) / Size);
+				float WI = FMath::Cos(2.f * PI * float(k * Blocks) / Size);
+
+				int32 Offset1 = 4 * (i1 + i * Size);
+
+				OutTable[Offset1 + 0] = j1;
+				OutTable[Offset1 + 1] = j2;
+				OutTable[Offset1 + 2] = WR;
+				OutTable[Offset1 + 3] = WI;
+
+				int32 Offset2 = 4 * (i2 + i * Size);
+
+				OutTable[Offset2 + 0] = j1;
+				OutTable[Offset2 + 1] = j2;
+				OutTable[Offset2 + 2] = -WR;
+				OutTable[Offset2 + 3] = -WI;
+			}
+		}
+	}
+}
