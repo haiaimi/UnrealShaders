c6e5f38bdc24abc174e0c525c6a42fd5e6e39bf2
haiaimi <wanghai1997@outlook.com>
2020-02-23 20:12:36 +0800

    Add triangle lerp test func

--- LearnProjects/EasyCustomShader/Plugins/UsingShaders/Source/UsingShaders/Public/GenerateWaveFunctionLibrary.h
+++ LearnProjects/EasyCustomShader/Plugins/UsingShaders/Source/UsingShaders/Public/GenerateWaveFunctionLibrary.h
@@ -19,7 +19,7 @@ class UGenerateWaveFunctionLibrary : public UBlueprintFunctionLibrary
 	
 	// Test trinangle normal lerp
 	UFUNCTION(BlueprintCallable, Category = "WaveGenerator", meta = (WorldContext = "WorldContextObject"))
-	static void TriangleNormalInterpolation(const UObject* WorldContextObject, const FVector& FirstVec, const FVector& SecondVec, const FVector& ThirdVec);
+	static void TriangleNormalInterpolation(const UObject* WorldContextObject, int32 InterpolationNum, const FVector& FirstVec, const FVector& SecondVec, const FVector& ThirdVec, const FVector& FirstColor, const FVector& SecondColor, const FVector& ThirdColor);
 
 	// Test quad normal lerp
 	UFUNCTION(BlueprintCallable, Category = "WaveGenerator", meta = (WorldContext = "WorldContextObject"))
