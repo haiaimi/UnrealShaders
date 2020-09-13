47707f3702d7945cae4e79f76f631f3c9fe99559
王海 <wanghai1997@outlook.com>
2020-09-14 01:25:29 +0800

    Add volume box vertices and indices

--- LearnProjects/FluidSimulation/Source/FluidSimulation/Private/RenderFluidVolume.cpp
+++ LearnProjects/FluidSimulation/Source/FluidSimulation/Private/RenderFluidVolume.cpp
@@ -14,7 +14,7 @@
 
 class FFluidVolumeBackPS : public FGlobalShader
 {
-	DECLARE_SHADER_TYPE(FFluidVolumeBack, Global);
+	DECLARE_SHADER_TYPE(FFluidVolumeBackPS, Global);
 
 	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
 	{
@@ -54,6 +54,52 @@ public:
 
 IMPLEMENT_SHADER_TYPE(, FFluidVolumeFrontPS, TEXT("/Shaders/Private/Fluid3D.usf"), TEXT("VolumeFrontPS"), SF_Pixel);
 
+
+void DrawVolumeBox(FRHICommandListImmediate& RHICmdList, TRefCountPtr<IPooledRenderTarget> RenderTarget, const FViewInfo& View)
+{
+	static TArray<FVector> Vertices = { FVector(0, 0, 0), 
+										FVector(0, 1, 0),
+										FVector(1, 0 ,0),
+										FVector(1, 1, 0),
+										FVector(0, 0, 1),
+										FVector(0, 1, 1),
+										FVector(1, 0, 1),
+										FVector(1, 1, 1)};
+
+	static TArray<int32> Indices = {0, 4, 5,  5, 1, 0, 
+									1, 5, 3,  5, 7, 3,
+									3, 7, 6,  6, 2, 3,
+									6, 4, 0,  0, 2, 6,
+									0, 1, 3,  3, 2, 0,
+									4, 6, 7,  7, 5, 4};
+
+	
+	FRHIRenderPassInfo RPInfo(RenderTarget->GetRenderTargetItem().TargetableTexture, ERenderTargetActions::Clear_DontStore);
+	RHICmdList.BeginRenderPass(RPInfo, TEXT("DrawVolumeBox"));
+	{
+		/*FIntRect SrcRect = View.ViewRect;
+		FIntRect DestRect = View.ViewRect;
+
+		RHICmdList.SetViewport()
+
+		FGraphicsPipelineStateInitializer GraphicsPSOInit;
+		Context.RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
+		GraphicsPSOInit.BlendState = TStaticBlendState<>::GetRHI();
+		GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
+		GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();
+
+		TShaderMapRef<FPostProcessVS> VertexShader(View.ShaderMap);
+		TShaderMapRef<FDistortionMergePS_ES2> PixelShader(View.ShaderMap);
+
+		GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GFilterVertexDeclaration.VertexDeclarationRHI;
+		GraphicsPSOInit.BoundShaderState.VertexShaderRHI = VertexShader.GetVertexShader();
+		GraphicsPSOInit.BoundShaderState.PixelShaderRHI = PixelShader.GetPixelShader();
+		GraphicsPSOInit.PrimitiveType = PT_TriangleList;
+
+		SetGraphicsPipelineState(Context.RHICmdList, GraphicsPSOInit);*/
+	}
+	RHICmdList.EndRenderPass();
+}
 
 void RenderFluidVolume(FRHICommandListImmediate& RHICmdList, FIntVector FluidVolumeSize, ERHIFeatureLevel::Type FeatureLevel)
 {
